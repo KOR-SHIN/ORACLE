0baeabf6a4ff1e99cee164f6c166d56b911d5ec4
KwangJin <shinkwang95@gmail.com>
2020-08-27 20:20:10 +0900

    [add] UNION ALL, INTERSECT, MINUS

--- BASIC/2020-08-26-set-operator.sql
+++ BASIC/2020-08-26-set-operator.sql
@@ -14,7 +14,7 @@
         
         
         
-        1-2. UNION
+        1-1. UNION
             합집합의 결과를 반환한다.
             사용형식 : SELECT 컬럼LIST 
                         FROM 테이블명1
@@ -112,3 +112,150 @@
                        AND BUY_DATE BETWEEN '20050501' AND0
                        '20050531'
                   GROUP BY BUY_PROD, PROD_NAME
+                  
+                  
+                  
+                  
+        1-2. UNION ALL
+            교차되는 부분(중복)의 모든자료를 반환한다.
+            ORDER BY는 마지막 query문에 사용한다.
+            
+                ex. 회원테이블에서 거주지가 '대전'이거나 여성회원이고 마일리지가 2000이상인 회원을 조회하시오.
+                    (단 Alias는 회원이름, 주소, 성별, 마일리지이다)
+                    
+                solution.
+                    SELECT MEM_NAME AS 회원이름,
+                           MEM_ADD1||' '||MEM_ADD2 AS 주소,
+                           CASE WHEN SUBSTR(MEM_REGNO2, 1, 1) = '1' THEN '남성'
+                                ELSE '여성' END AS 성별,
+                           MEM_MILEAGE AS 마일리지
+                      FROM MEMBER
+                     WHERE MEM_ADD1 LIKE '대전%'
+                 UNION ALL
+                 
+                    SELECT MEM_NAME AS 회원이름,
+                           MEM_ADD1||' '||MEM_ADD2 AS 주소,
+                           CASE WHEN SUBSTR(MEM_REGNO2, 1, 1) = '1' THEN '남성'
+                                ELSE '여성' END AS 성별,
+                           MEM_MILEAGE AS 마일리지
+                      FROM MEMBER
+                     WHERE SUBSTR(MEM_REGNO2, 1, 1) = '2'
+                 UNION ALL
+                     
+                     SELECT MEM_NAME AS 회원이름,
+                               MEM_ADD1||' '||MEM_ADD2 AS 주소,
+                               CASE WHEN SUBSTR(MEM_REGNO2, 1, 1) = '1' THEN '남성'
+                                    ELSE '여성' END AS 성별,
+                               MEM_MILEAGE AS 마일리지
+                          FROM MEMBER
+                         WHERE MEM_MILEAGE >= 2000
+                      ORDER BY 1
+                      
+                      
+                    
+        1-3. INTERSECT
+            복수개의 쿼리 결과 중 교차되는 부분만 반환시키는 집합연산자
+            
+                ex. 장바구니테이블에서 4월과 6월에 모두 판매된 상품을 조회하시오.
+                    (단, Alias는 상품코드, 상품명이다)
+                    
+                solution 1. 4월 자료
+                    -- 한 상품이 여러개 팔리는 경우가 있기때문에 중복을 제거해야한다.
+                    SELECT DISTINCT(A.CART_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM CART A, PROD B
+                     WHERE A.CART_PROD = B.PROD_ID
+                       AND A.CART_NO LIKE '200504%'
+                       
+                solution 2. 6월 자료
+                    SELECT DISTINCT(A.CART_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM CART A, PROD B
+                     WHERE A.CART_PROD = B.PROD_ID
+                       AND A.CART_NO LIKE '200506%'
+                       
+                solution 3. INTERSECT로 교집합 구하기
+                    SELECT DISTINCT(A.CART_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM CART A, PROD B
+                     WHERE A.CART_PROD = B.PROD_ID
+                       AND A.CART_NO LIKE '200504%'
+                 INTERSECT
+                 
+                 SELECT DISTINCT(A.CART_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM CART A, PROD B
+                     WHERE A.CART_PROD = B.PROD_ID
+                       AND A.CART_NO LIKE '200506%'
+                  
+                  solution 4. EXISTS 사용하기
+                    SELECT DISTINCT(A.CART_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM CART A, PROD B
+                     WHERE A.CART_PROD = B.PROD_ID
+                       AND A.CART_NO LIKE '200504%'
+                       AND EXISTS (
+                                    SELECT 1
+                                      FROM CART C
+                                     WHERE C.CART_PROD = A.CART_PROD
+                                       AND C.CART_NO LIKE '200506%'
+                                  )
+                                  
+                   etc. 4월에는 판매되었지만, 6월에는 판매되지 않은 상품
+                    SELECT DISTINCT(A.CART_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM CART A, PROD B
+                     WHERE A.CART_PROD = B.PROD_ID
+                       AND A.CART_NO LIKE '200504%'
+                       AND NOT EXISTS (
+                                    SELECT 1
+                                      FROM CART C
+                                     WHERE C.CART_PROD = A.CART_PROD
+                                       AND C.CART_NO LIKE '200506%'
+                                  )
+                  
+                  
+        1-4. MINUS
+            복수개 쿼리 결과의 차집합을 반환하는 집합연산자
+            
+                ex. 매입테이블에서 2월에 입고된 상품 중 4월에는 입고되지 않은 상품을 조회하시오.
+                    (단, Alias는 상품코드와 상품명이다)
+                    
+                solution 1. 2월에 입고된 상품
+                    SELECT DISTINCT(A.BUY_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM BUYPROD A, PROD B
+                     WHERE A.BUY_PROD = B.PROD_ID
+                       AND A.BUY_DATE BETWEEN '20050201' AND '20050228'
+                       
+                solution 2. 4월에 입고된 상품
+                    SELECT DISTINCT(A.BUY_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM BUYPROD A, PROD B
+                     WHERE A.BUY_PROD = B.PROD_ID
+                       AND A.BUY_DATE BETWEEN '20050401' AND '20050430'
+                  
+                solution 3. 
+                    SELECT DISTINCT(A.BUY_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM BUYPROD A, PROD B
+                     WHERE A.BUY_PROD = B.PROD_ID
+                       AND A.BUY_DATE BETWEEN '20050201' AND '20050228'
+                    MINUS
+                    
+                    SELECT DISTINCT(A.BUY_PROD) AS 상품코드,
+                           B.PROD_NAME AS 상품명
+                      FROM BUYPROD A, PROD B
+                     WHERE A.BUY_PROD = B.PROD_ID
+                       AND A.BUY_DATE BETWEEN '20050401' AND '20050430'
+                  ORDER BY 1
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
